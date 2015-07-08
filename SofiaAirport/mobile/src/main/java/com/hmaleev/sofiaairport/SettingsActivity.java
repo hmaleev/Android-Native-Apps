595c2d59d782b03236612c7c1f3bc9f9c34329c9
Hristo <hmaleev@gmail.com>
2015-07-08 22:46:14 +0300

    updated Sofia Airport
    fixed bug when displaying settings for 7+ inch tablets

--- SofiaAirport/mobile/src/main/java/com/hmaleev/sofiaairport/SettingsActivity.java
+++ SofiaAirport/mobile/src/main/java/com/hmaleev/sofiaairport/SettingsActivity.java
@@ -1,8 +1,11 @@
 package com.hmaleev.sofiaairport;
 
+import android.annotation.TargetApi;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
@@ -14,10 +17,9 @@ import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
 import android.text.TextUtils;
 
+import com.hmaleev.sofiaairport.R;
 
 import java.util.List;
-
-import static android.app.PendingIntent.getActivity;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
@@ -30,95 +32,26 @@ import static android.app.PendingIntent.getActivity;
  * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
  * API Guide</a> for more information on developing a Settings UI.
  */
-public class SettingsActivity extends PreferenceActivity /*implements SharedPreferences.OnSharedPreferenceChangeListener*/ {
-    /**
-     * Determines whether to always show the simplified settings UI, where
-     * settings are presented in a single list. When false, settings are shown
-     * as a master/detail two-pane view on tablets. When true, a single pane is
-     * shown on tablets.
-     */
+public class SettingsActivity extends PreferenceActivity {
 
-
- /*   public static final String CHANGE_DEF_ = "pref_syncConnectionType";
-*/
-  /*  public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-                                          String key) {
-        if (key.equals(PREF_FLIGHT_COUNT)) {
-            Preference connectionPref = findPreference(key);
-            // Set summary to be the user-description for the selected value
-            connectionPref.setSummary(sharedPreferences.getString(key, ""));
-
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putInt(PREF_FLIGHT_COUNT, 2);
-            editor.commit();
-        }
-    }
-
-
-
-*/
-
-
-
-   // private static final boolean ALWAYS_SIMPLE_PREFS = false;
     public static  final  String PREF_FLIGHT_COUNT = "flight_count";
     public static  final  String PREF_AUTO_SYNC = "auto_sync_enable";
     public static  final  String PREF_AUTO_SYNC_INTERVAL = "auto_sync_interval";
-  //  public static final String PREF_FLIGHT_COUNT = "com.hmaleev.sofiaairport.FLIGHT_COUNT";
-
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        Context ctx = getApplicationContext();
-        setupSimplePreferencesScreen();
-
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
     }
 
-    /**
-     * Shows the simplified settings UI if the device configuration if the
-     * device configuration dictates that a simplified, single-pane UI should be
-     * shown.
-     */
-    private void setupSimplePreferencesScreen() {
-        if (!isSimplePreferences(this)) {
-            return;
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_data_sync);
         }
-
-        // In the simplified UI, fragments are not used at all and we instead
-        // use the older PreferenceActivity APIs.
-
-
-        // Add 'general' preferences.
-        addPreferencesFromResource(R.xml.pref_data_sync);
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean onIsMultiPane() {
-        return isXLargeTablet(this) && !isSimplePreferences(this);
-    }
-
-    /**
-     * Helper method to determine if the device has an extra-large screen. For
-     * example, 10" tablets are extra-large.
-     */
-    private static boolean isXLargeTablet(Context context) {
-        return (context.getResources().getConfiguration().screenLayout
-                & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
-    }
-
-    /**
-     * Determines whether the simplified settings UI should be shown. This is
-     * true if this is forced via {@link #ALWAYS_SIMPLE_PREFS}, or the device
-     * doesn't have newer APIs like {@link PreferenceFragment}, or the device
-     * doesn't have an extra-large screen. In these cases, a single-pane
-     * "simplified" settings UI should be shown.
-     */
-    private static boolean isSimplePreferences(Context context) {
-        return Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB
-                || !isXLargeTablet(context);
-    }
 }
+
