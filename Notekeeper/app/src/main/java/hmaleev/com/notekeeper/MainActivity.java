a576832de02b506970fe5363f469248d68d9352a
Hristo <hmaleev@gmail.com>
2018-01-25 22:35:19 +0200

    updated Notekeeper
    Improved UI

--- Notekeeper/app/src/main/java/hmaleev/com/notekeeper/MainActivity.java
+++ Notekeeper/app/src/main/java/hmaleev/com/notekeeper/MainActivity.java
@@ -8,12 +8,24 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.List;
+
+import hmaleev.com.notekeeper.DAO.NoteDao;
+import hmaleev.com.notekeeper.Models.Note;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private RecyclerView mRecyclerView;
+    List<Note> contacts;
+    AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +33,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        db = AppDatabase.getAppDatabase(MainActivity.this);
+        mRecyclerView = (RecyclerView) findViewById(R.id.noteList);
+        // Lookup the recyclerview in activity layout
+
+        // Initialize contacts
+        contacts = db.noteDao().getAll();
+        // Create adapter passing in the sample user data
+
+        NoteAdapter adapter = new NoteAdapter(this,contacts);
+        mRecyclerView.setAdapter(adapter);
+
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        // Set layout manager to position the items
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
 
         FloatingActionButton fab = findViewById(R.id.addNoteBtn);
         fab.setOnClickListener(new View.OnClickListener() {
---
+++ Notekeeper/app/src/main/java/hmaleev/com/notekeeper/NoteAdapter.java
@@ -0,0 +1,92 @@
+package hmaleev.com.notekeeper;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import hmaleev.com.notekeeper.Models.Note;
+
+/**
+ * Created by Xristo on 23.1.2018 г..
+ */
+public class NoteAdapter extends
+        RecyclerView.Adapter<NoteAdapter.ViewHolder> {
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        public TextView title;
+        public TextView noteContent;
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(View itemView) {
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+
+            title = (TextView) itemView.findViewById(R.id.noteListTitle);
+            noteContent = (TextView) itemView.findViewById(R.id.noteListContent);
+        }
+    }
+
+    // ... view holder defined above...
+
+    // Store a member variable for the contacts
+    private List<Note> notes;
+    // Store the context for easy access
+    private Context mContext;
+
+    // Pass in the contact array into the constructor
+    public NoteAdapter(Context context, List<Note> contacts) {
+        notes = contacts;
+        mContext = context;
+    }
+
+    // Easy access to the context object in the recyclerview
+    private Context getContext() {
+        return mContext;
+    }
+
+    // ... constructor and member variables
+
+    // Usually involves inflating a layout from XML and returning the holder
+    @Override
+    public NoteAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.recycler_view_main_template, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(contactView);
+        return viewHolder;
+    }
+
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(NoteAdapter.ViewHolder viewHolder, int position) {
+        // Get the data model based on position
+        Note contact = notes.get(position);
+
+        // Set item views based on your views and data model
+        TextView textView = viewHolder.title;
+        textView.setText(contact.getTitle());
+        TextView button = viewHolder.noteContent;
+        button.setText(contact.getContent());
+    }
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+}
